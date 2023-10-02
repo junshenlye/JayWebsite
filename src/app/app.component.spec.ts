a78a26fb586efd9bea67964b37e81af4e786c74e
ItsDaMeMario <junshenlye@gmail.com>
2023-10-03 01:37:23 +0800

    init angular routing and tailwind css

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'jay_website'`, () => {
+  it(`should have as title 'JayWebsite'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('jay_website');
+    expect(app.title).toEqual('JayWebsite');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('jay_website app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('JayWebsite app is running!');
   });
 });
